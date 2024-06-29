9e0c6140cb55d47b66c916d153ca081ba379076a
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2024-06-29 20:06:38 +0530

    updated socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -1,5 +1,6 @@
 const { Server } = require("socket.io");
 const http = require("http");
+const cors = require("cors");
 
 const express = require("express");
 
@@ -9,10 +10,16 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:10000"],
     methods: ["GET", "POST"],
   },
 });
+
+
+app.use(cors({
+  origin: 'http://localhost:10000',
+  methods: ['GET', 'POST'],
+}));
 
 const getReceiverSocketId = (receiverId) => {
 
